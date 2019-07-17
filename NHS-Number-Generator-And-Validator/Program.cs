04fdb5d133c153ba5275e2326d5cb5df3a5e7461
Vlad-Alexandru Tănăsescu <vatanasescu@outlook.com>
2019-07-17 10:49:01 +0100

    Create Solution

---
+++ NHS-Number-Generator-And-Validator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NHS_Number_Generator_And_Validator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
